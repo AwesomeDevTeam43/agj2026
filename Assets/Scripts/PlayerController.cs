ea0dac8bf4db512ce54e10c5e3537fa4aa04d943
João Manuel <148542897+Jo6o43@users.noreply.github.com>
2026-03-21 00:49:53 +0000

    Rotation 360

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,12 +18,27 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Movement();
-    }   
+        Rotation();
+    }
 
     void Movement()
     {
         Vector2 movement = new Vector2(input.MovementInput.x, input.MovementInput.y);
         movement.Normalize();
         rb.linearVelocity = movement * moveSpeed;
+    }
+
+    void Rotation()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f));
+
+        Vector2 direction = new Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y);
+
+        // 3. Use Atan2 to find the angle in radians, then convert to degrees
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        // 4. Apply the rotation
+        // Adjust the "- 90f" depending on which way your sprite's "front" faces in the PNG
+        transform.rotation = Quaternion.Euler(0, 0, angle - 90f);
     }
 }

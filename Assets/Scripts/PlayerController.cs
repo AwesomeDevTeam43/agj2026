fe1a5f350559069d08102a95412a0c3328105228
João Manuel <148542897+Jo6o43@users.noreply.github.com>
2026-03-21 00:58:41 +0000

    Add Post Processing Bloom

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -31,14 +31,8 @@ public class PlayerController : MonoBehaviour
     void Rotation()
     {
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f));
-
         Vector2 direction = new Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y);
-
-        // 3. Use Atan2 to find the angle in radians, then convert to degrees
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-
-        // 4. Apply the rotation
-        // Adjust the "- 90f" depending on which way your sprite's "front" faces in the PNG
         transform.rotation = Quaternion.Euler(0, 0, angle - 90f);
     }
 }

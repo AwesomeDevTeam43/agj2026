75386df9c42bcd174b8d2e84de524d798969a99d
tiago <tmdmiranda@gmail.com>
2026-03-21 11:56:26 +0000

    comite

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -3,11 +3,20 @@ using UnityEngine;
 public class Test : MonoBehaviour, IInteractable
 {
     [SerializeField] private ShapeVisualizer playerController;
+    private ShapeVisualizer shapeVisualizer;
     [SerializeField] private ShapeData shapeData;
 
     void Awake()
     {
-        
+        shapeVisualizer = GetComponent<ShapeVisualizer>();
+    }
+
+    void Start()
+    {
+        if (shapeData != null)
+        {
+            shapeVisualizer.ApplyShape(shapeData);
+        }
     }
 
     public void OnClick()

2b66b141711c0eca1faf08d6271feeaf6dee063e
João Manuel <148542897+Jo6o43@users.noreply.github.com>
2026-03-22 11:20:01 +0000

    Revert "Git aaa"
    This reverts commit 386f572b188a0b4ea873706795bcd474a638497e.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
-using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -21,9 +20,6 @@ public class GameManager : MonoBehaviour
     
     [SerializeField] private string checkBox = "[X]";
     [SerializeField] private string uncheckBox = "[ ]";
-
-    [Header("Suspicion Bar")]
-    [SerializeField] private Slider _suspicionBar;
 
     private bool _isGameOver = false;
 
